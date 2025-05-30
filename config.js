67789caaff4e4cb5ad7906f731122d3df8bc8795
sanjusharmagame <sanjusharmagame@gmail.com>
2025-05-30 15:31:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBrquk8FdPXfheFIuwhlBT3IEWpHe7-iWE",
+  authDomain: "gopalgame-13553.firebaseapp.com",
+  projectId: "gopalgame-13553",
+  storageBucket: "gopalgame-13553.firebasestorage.app",
+  messagingSenderId: "114051811205",
+  appId: "1:114051811205:web:9282e9d0314f8ad95166ec",
+  measurementId: "G-V52EEYB6W4"
+};
+export const app = initializeApp(firebaseConfig);
